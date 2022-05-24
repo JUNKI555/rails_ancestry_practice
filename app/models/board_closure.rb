00d18a9af72074b8ec915ed7ae7b7e83d0882bd2
JUNKI555 <junki555@gmail.com>
2022-05-24 12:30:37 +0900

    :tada: 必要だが標準に不足しているscopeなどを追加

--- app/models/board_closure.rb
+++ app/models/board_closure.rb
@@ -15,4 +15,32 @@
 class BoardClosure < ApplicationRecord
   has_closure_tree name_column: 'board_name'
   has_many :epic_closures
+
+  # 親でもないし子孫でもない
+  scope :not_parents_and_not_children, -> {
+    where(parent_id: nil).where("NOT EXISTS (#{ancestor_hierarchy_sql})")
+  }
+
+  # 親である
+  scope :parents, -> {
+    where("EXISTS (#{ancestor_hierarchy_sql})")
+  }
+
+  # 親、もしくは子孫である
+  scope :parents_or_children, -> {
+    where.not(parent_id: nil).or(BoardClosure.where("EXISTS (#{ancestor_hierarchy_sql})"))
+  }
+
+  # EXISTS/NOT EXISTS で使う用の、1世代以降のhierarchyが存在するかのSQLを生成
+  def self.ancestor_hierarchy_sql
+    BoardClosure.hierarchy_class
+                .select(1)
+                .where('`ancestor_id` = `board_closures`.`id`')
+                .where('`generations` > 0')
+                .to_sql
+  end
+
+  def belong_hash_tree
+    self.root.hash_tree
+  end
 end
