70f45ca9e3696361a9e16417cab36e80fd848678
JUNKI555 <junki555@gmail.com>
2022-05-24 12:30:12 +0900

    :pencil2: scopeなどにコメントを追加

--- app/models/board_ancestry.rb
+++ app/models/board_ancestry.rb
@@ -17,12 +17,20 @@ class BoardAncestry < ApplicationRecord
   has_ancestry orphan_strategy: :rootify, cache_depth: true
   has_many :epic_ancestries
 
+  # 末端である
   scope :leaves, -> { joins("LEFT JOIN board_ancestries AS c ON c.ancestry = CAST(board_ancestries.id AS char(50)) OR c.ancestry = concat(board_ancestries.ancestry, '/', board_ancestries.id)").group("board_ancestries.id").having('COUNT(c.id) = 0') }
+
+  # 親でもないし子孫でもない(rootかつ末端)(rootsは親が含まれるので末端であるかどうかで親を除外)
   scope :not_parents_and_not_children, -> { roots.leaves }
+
+  # 親である
   scope :parents, -> { where(id: BoardAncestry.select(:ancestry).distinct.pluck(:ancestry).compact.map { |x| x.split('/') }.flatten.uniq) }
+
+  # 親、もしくは子孫である
   scope :parents_or_children, -> { parents.or(BoardAncestry.where.not(ancestry: nil)) }
 
-  def belong_subtree
+  # nodeの全ての子孫
+  def belong_sub_tree
     self.root.subtree
   end
 end
